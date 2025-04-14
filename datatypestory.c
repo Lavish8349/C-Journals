ab0de4afed205e3b00f3653cc5162d7286bb64d5
Lavish Gaur <lavishgaur06902@gmail.com>
2025-04-15 04:52:40 +0530

    Add files via upload

---
+++ datatypestory.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char characterName[] = "Lavish";
+    int characterAge = 18;
+    char universityName[] = "MEDICAPS UNIVERSITY";
+    double cgpa = 9.0;
+
+    printf("There was a boy named %s\n", characterName);
+    printf("He opted for commerce in his 11th grade\n");
+    printf("He wasn't happy about that decision a year later\n");
+    printf("And he wanted to persue tech and coding\n");
+    printf("So that %d year old boy took a big desicion\n", characterAge);
+    printf("He decided to persue BCA\n");
+    printf("And currently he is writing code\n");
+    printf("He took admission in %s\n", universityName);
+    printf("He is manifesting to get a CGPA of %.1f\n", cgpa);
+    printf("Thats it for the journal today\n");
+
+    return 0;
+
+}
---
+++ storyvariable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char characterName[] = "Lavish";
+    int characterAge = 18;
+    char characterHobby[] = "Typing";
+
+    printf("There was a boy named %s\n", characterName);
+    printf("He was %d years old\n", characterAge);
+    printf("He loved %s\n", characterHobby);
+    printf("His obsession with %s was so great that he was doing it all day\n", characterHobby);
+    printf("He went from typing on his phone to typing on his laptop\n");
+    printf("It was his first ever laptop\n");
+    printf("He went from 20 words per minute to 93 words per minute IN 10WORDS\n");
+    printf("That too in just 18 days\n");
+
+    return 0;
+}
